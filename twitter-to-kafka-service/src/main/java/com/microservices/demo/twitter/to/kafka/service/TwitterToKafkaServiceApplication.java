ef661e2c7af831b7b703045bf014ca56617b1fb4
kyroxen <eishanjindal88@gmail.com>
2021-12-04 17:08:16 +0530

    Configure a multi module project

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,13 +1,12 @@
-package com.microservices.demo;
+package com.microservices.demo.twitter.to.kafka.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroservicesDemoApplication {
+public class TwitterToKafkaServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroservicesDemoApplication.class, args);
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
     }
-
 }
